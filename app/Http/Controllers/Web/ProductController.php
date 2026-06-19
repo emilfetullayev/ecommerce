8b0b1b68500ea38a62055a2f10787d6286d5b976
Emil Fetullayev <emil.fatullayev.1996@gmail.com>
2026-06-20 00:43:14 +0400

    category fix

--- app/Http/Controllers/Web/ProductController.php
+++ app/Http/Controllers/Web/ProductController.php
@@ -109,31 +109,24 @@ class ProductController extends Controller
 
         // 🔥 AJAX SUPPORT (index ilə eyni sistem)
         if ($request->ajax()) {
-
             $html = '';
-
             foreach ($products as $data) {
-
-                $translation = $data->translations->first();
+                $translation = $data->translations->where('locale', app()->getLocale())->first()
+                    ?? $data->translations->where('locale', 'az')->first();
 
                 $name = $translation->name ?? $data->name;
-
-                // 💰 price logic (company varsa wholesale)
                 $price = $data->retail_price;
 
-                if (auth()->guard('company')->check() &&
-                    auth()->guard('company')->user()->price_type === 'wholesale') {
+                if (auth()->guard('company')->check() && auth()->guard('company')->user()->price_type === 'wholesale') {
                     $price = $data->wholesale_price;
                 }
 
                 $img = optional($data->images->first())->image;
                 $imgSrc = $img ? asset('storage/' . $img) : asset('web/image/no-image.png');
-
-                $categoryName = $data->category?->translations
-                    ->firstWhere('locale', app()->getLocale())?->name
-                    ?? $data->category?->translations
-                        ->firstWhere('locale', 'az')?->name;
-
+                $categoryName = $data->category?->translations->firstWhere('locale', app()->getLocale())?->name
+                    ?? $data->category?->translations->firstWhere('locale', 'az')?->name;
+                $productCode = $data->code ?? '';
+                $formattedPrice = number_format($price, 2);
                 $productUrl = route('web.product.show', $data->id);
                 $loginUrl = route('company.login');
 
@@ -143,38 +136,41 @@ class ProductController extends Controller
 
                 $html .= '
 <div class="col-xs-12 col-sm-6 col-md-3 product-layout">
-    <div class="grainger-product-card" data-product-id="' . $data->id . '">
-
+    <div class="grainger-product-card style-ad-card" data-product-id="' . $data->id . '">
         <div class="ad-image-box">
             <a href="' . $productUrl . '">
-                <img src="' . $imgSrc . '" class="img-responsive">
+                <img src="' . $imgSrc . '" class="img-responsive" alt="' . $name . '">
             </a>
+            <div class="ad-heart-badge"><i class="fa fa-heart-o"></i></div>
         </div>
-
         <div class="grainger-info-wrapper">
-
-            <span class="grainger-brand">' . $categoryName . '</span>
-
-            <h4 class="grainger-title">
-                <a href="' . $productUrl . '">' . $name . '</a>
-            </h4>
-
-            <div class="grainger-price-block">
-                <span class="price-amount">' . number_format($price, 2) . ' ₼</span>
+            <div class="grainger-top-meta">
+                <span class="grainger-brand">' . $categoryName . '</span>
+                <h4 class="grainger-title"><a href="' . $productUrl . '">' . $name . '</a></h4>
+                <div class="grainger-sku">Məhsul kodu ' . $productCode . '</div>
             </div>
-
-            ' . $cartButton . '
-
+            <div class="grainger-price-block">
+                <span class="price-label">Qiyməti</span>
+                <div class="price-row">
+                    <span class="price-amount">' . $formattedPrice . ' ₼</span>
+                </div>
+            </div>
+            <div class="grainger-action-block">
+                <div class="quantity-wrapper">
+                    <div class="qty-control-group">
+                        <button type="button" class="qty-btn grainger-qty-minus">-</button>
+                        <input type="number" value="1" min="1" class="qty-input grainger-qty-input">
+                        <button type="button" class="qty-btn grainger-qty-plus">+</button>
+                    </div>
+                </div>
+                ' . $cartButton . '
+            </div>
         </div>
-
     </div>
 </div>';
             }
 
-            return response()->json([
-                'html' => $html,
-                'hasMore' => $products->hasMorePages()
-            ]);
+            return response()->json(['html' => $html, 'hasMore' => $products->hasMorePages()]);
         }
 
         return view('site.product.index', compact('products', 'category'));
